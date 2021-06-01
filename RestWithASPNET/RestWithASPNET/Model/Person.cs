4dc97b72a0904b1ee469dfa4f9ffb5e5d6475582
dvieirar <diegovieira@uel.br>
2021-06-01 09:14:26 -0300

    Working with four verbs

---
+++ RestWithASPNET/RestWithASPNET/Model/Person.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Model {
+    public class Person {
+        public long Id {get; set;}
+        public string firstName {get; set;}
+        public string lastName {get; set;}
+        public string Address {get; set;}
+        public string Gender {get; set;}
+    }
+}
